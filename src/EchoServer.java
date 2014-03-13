089bce1a0d20ca21a8a04b827d72ff1777a1ac4e
alexreedhill <ahill1994@student.elgin.edu>
2014-03-13 15:42:58 -0500

    Refactor EchoServer

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -6,36 +6,45 @@ import java.net.*;
 
 public class EchoServer {
     private static final int PORT = 8000;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public EchoServer() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+        clientSocket = serverSocket.accept();
+        OutputStream clientOutputStream = clientSocket.getOutputStream();
+        out = new PrintWriter(clientOutputStream, true);
+        InputStream clientInputStream = clientSocket.getInputStream();
+        in = new BufferedReader(new InputStreamReader(clientInputStream));
+    }
 
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        OutputStream clientOutputStream = null;
-        InputStream clientInputStream = null;
-        String inputLine = null;
-        Socket clientSocket = null;
-        PrintWriter out = null;
-
-        try {
-            serverSocket = new ServerSocket(PORT);
-            clientSocket = serverSocket.accept();
-            clientOutputStream = clientSocket.getOutputStream();
-            out = new PrintWriter(clientOutputStream, true);
-            clientInputStream = clientSocket.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientInputStream));
-            System.out.println("Echo server started");
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("echoing: " + inputLine);
-                out.println(inputLine);
-            }
-            out.close();
-            in.close();
-            clientSocket.close();
-            serverSocket.close();
+        try  {
+            EchoServer server = new EchoServer();
+            server.run();
         } catch(IOException ex) {
             System.err.println(ex);
             System.exit(1);
         }
+    }
 
+    public void run() throws IOException {
+        System.out.println("Echo server started");
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("echoing: " + inputLine);
+            out.println(inputLine);
+        }
+        stop();
+    }
+
+    private void stop() throws IOException {
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 
 }
